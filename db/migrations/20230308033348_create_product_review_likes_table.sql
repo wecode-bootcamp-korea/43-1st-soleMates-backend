98cbd109e0a006dfcd259454d80b50ff222081df
해동북 <hodozi@haedongbug-ui-MacBook-Pro.local>
2023-03-08 17:50:57 +0900

    [Modify] dbmate up 오류 수정

--- db/migrations/20230308033348_create_product_review_likes_table.sql
+++ db/migrations/20230308033348_create_product_review_likes_table.sql
@@ -1,11 +1,11 @@
 -- migrate:up
-CREATE TABLE product_reviews_likes(
+CREATE TABLE product_review_likes(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     product_review_id INT NOT NULL,
-    CONSTRAINT product_reviews_likes_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id),
-    CONSTRAINT product_reviews_likes_product_review_id_fkey FOREIGN KEY (product_review_id) REFERENCES product_reivew(id),
+    CONSTRAINT product_review_likes_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT product_review_likes_product_review_id_fkey FOREIGN KEY (product_review_id) REFERENCES product_reviews(id),
     CONSTRAINT review_likes_only UNIQUE (user_id, product_review_id)
 );
 -- migrate:down
-DROP TABLE product_reviews_likes;
+DROP TABLE product_review_likes;
